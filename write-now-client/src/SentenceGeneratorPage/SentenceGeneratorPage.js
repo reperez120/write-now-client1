989a02fc9fee798d6167a4b16dbd93425a49b411
Rachel Perez <46304321+reperez120@users.noreply.github.com>
2019-07-30 01:44:44 -0400

    Update SentenceGeneratorPage.js

--- write-now-client/src/SentenceGeneratorPage/SentenceGeneratorPage.js
+++ write-now-client/src/SentenceGeneratorPage/SentenceGeneratorPage.js
@@ -19,13 +19,24 @@ class SentenceGeneratorPage extends Component {
           verb: [],
           preposition: [],
           location: [],
-          punctation: []
+          punctation: [],
+          genre: '',
+          loading: false
         };
+      }
+
+      handleChange = e => {
+        this.setState({
+          genre: e.target.value
+        });
       }
     
     handleFormSubmit = e => {
 
         e.preventDefault()
+        console.log(this.state.genre)
+
+        this.setState({ loading: true })
     
         const wordsAPI = 'https://calm-hamlet-15286.herokuapp.com/words?type=';
         const subURL = 'https://calm-hamlet-15286.herokuapp.com/words?type=subject';
@@ -41,64 +52,91 @@ class SentenceGeneratorPage extends Component {
             method: 'GET',
           }
 
-          fetch(locAdjURL,  options)
+          fetch(verbURL,  options)
           .then(response => response.json())
           .then(data => {
-              let locadj = data[Math.floor(Math.random() * data.length)]
-              let locationadjective = locadj.word
+              let ver = data[Math.floor(Math.random() * data.length)]
+              let verb = ver.word
 
               this.setState({
                   firstArticle: 'The',
+                  verb,
                   article: 'the',
                   punctation: '.',
-                  locationadjective
+                  loading: false
               });
-          })  
+          }) 
 
-          fetch(subAdjURL,  options)
+          fetch(prepURL, options)
           .then(response => response.json())
           .then(data => {
-              let subadj = data[Math.floor(Math.random() * data.length)]
-              let subjectadjective = subadj.word
+              let prep = data[Math.floor(Math.random() * data.length)]
+              let preposition = prep.word
 
               this.setState({
-                  subjectadjective
+                  preposition,
               });
-          })  
+          }) 
 
-            fetch(subURL,  options)
+          if (this.state.genre == 'horror') {
+
+            fetch('http://localhost:8000/words?type=sub-adj&genre=horror', options)
+            .then(response => response.json())
+            .then(data => {
+                let subadj = data[Math.floor(Math.random() * data.length)]
+                let subjectadjective = subadj.word
+  
+                this.setState({
+                    subjectadjective
+                });
+            }) 
+
+            fetch('http://localhost:8000/words?type=subject&genre=horror', options)
             .then(response => response.json())
             .then(data => {
                 let sub = data[Math.floor(Math.random() * data.length)]
                 let subject = sub.word
-
+  
                 this.setState({
-                    article: 'the',
                     subject
                 });
             })  
 
-            fetch(verbURL,  options)
+            fetch('http://localhost:8000/words?type=loc-adj&genre=horror', options)
             .then(response => response.json())
             .then(data => {
-                let ver = data[Math.floor(Math.random() * data.length)]
-                let verb = ver.word
-
+                let locadj = data[Math.floor(Math.random() * data.length)]
+                let locationadjective = locadj.word
+  
                 this.setState({
-                    verb,
+                    locationadjective
                 });
             }) 
 
-            fetch(prepURL, options)
+                fetch('http://localhost:8000/words?type=loc-adj&genre=horror', options)
             .then(response => response.json())
             .then(data => {
-                let prep = data[Math.floor(Math.random() * data.length)]
-                let preposition = prep.word
-
+                let locadj = data[Math.floor(Math.random() * data.length)]
+                let locationadjective = locadj.word
+  
                 this.setState({
-                    preposition,
+                    locationadjective
                 });
             }) 
+         
+            fetch('http://localhost:8000/words?type=location&genre=horror', options)
+            .then(response => response.json())
+            .then(data => {
+                let loc = data[Math.floor(Math.random() * data.length)]
+                let location = loc.word
+  
+                this.setState({
+                    location
+                });
+            })  
+          }
+
+            if (this.state.genre == 'wildcard') {
 
             fetch(locURL, options)
             .then(response => response.json())
@@ -122,6 +160,277 @@ class SentenceGeneratorPage extends Component {
                     adjective,
                 });
             }) 
+
+            fetch(locAdjURL,  options)
+                .then(response => response.json())
+                .then(data => {
+                    let locadj = data[Math.floor(Math.random() * data.length)]
+                    let locationadjective = locadj.word
+
+                    this.setState({
+                        firstArticle: 'The',
+                        article: 'the',
+                        punctation: '.',
+                        locationadjective,
+                        loading: false
+                    });
+                })  
+
+                fetch(subAdjURL,  options)
+                .then(response => response.json())
+                .then(data => {
+                    let subadj = data[Math.floor(Math.random() * data.length)]
+                    let subjectadjective = subadj.word
+
+                    this.setState({
+                        subjectadjective
+                    });
+                })  
+
+                fetch(subURL,  options)
+                .then(response => response.json())
+                .then(data => {
+                    let sub = data[Math.floor(Math.random() * data.length)]
+                    let subject = sub.word
+
+                    this.setState({
+                        article: 'the',
+                        subject
+                    });
+                }) 
+            }
+            if (this.state.genre == 'fantasy') {
+
+                fetch('http://localhost:8000/words?type=sub-adj&genre=fantasy', options)
+                .then(response => response.json())
+                .then(data => {
+                    let subadj = data[Math.floor(Math.random() * data.length)]
+                    let subjectadjective = subadj.word
+      
+                    this.setState({
+                        subjectadjective
+                    });
+                }) 
+    
+                fetch('http://localhost:8000/words?type=subject&genre=fantasy', options)
+                .then(response => response.json())
+                .then(data => {
+                    let sub = data[Math.floor(Math.random() * data.length)]
+                    let subject = sub.word
+      
+                    this.setState({
+                        subject
+                    });
+                })  
+    
+                fetch('http://localhost:8000/words?type=loc-adj&genre=fantasy', options)
+                .then(response => response.json())
+                .then(data => {
+                    let locadj = data[Math.floor(Math.random() * data.length)]
+                    let locationadjective = locadj.word
+      
+                    this.setState({
+                        locationadjective
+                    });
+                }) 
+    
+                    fetch('http://localhost:8000/words?type=loc-adj&genre=fantasy', options)
+                .then(response => response.json())
+                .then(data => {
+                    let locadj = data[Math.floor(Math.random() * data.length)]
+                    let locationadjective = locadj.word
+      
+                    this.setState({
+                        locationadjective
+                    });
+                }) 
+             
+                fetch('http://localhost:8000/words?type=location&genre=fantasy', options)
+                .then(response => response.json())
+                .then(data => {
+                    let loc = data[Math.floor(Math.random() * data.length)]
+                    let location = loc.word
+      
+                    this.setState({
+                        location
+                    });
+                })              
+            }
+
+            if (this.state.genre == 'western') {
+
+                fetch('http://localhost:8000/words?type=sub-adj&genre=western', options)
+                .then(response => response.json())
+                .then(data => {
+                    let subadj = data[Math.floor(Math.random() * data.length)]
+                    let subjectadjective = subadj.word
+      
+                    this.setState({
+                        subjectadjective
+                    });
+                }) 
+    
+                fetch('http://localhost:8000/words?type=subject&genre=western', options)
+                .then(response => response.json())
+                .then(data => {
+                    let sub = data[Math.floor(Math.random() * data.length)]
+                    let subject = sub.word
+      
+                    this.setState({
+                        subject
+                    });
+                })  
+    
+                fetch('http://localhost:8000/words?type=loc-adj&genre=western', options)
+                .then(response => response.json())
+                .then(data => {
+                    let locadj = data[Math.floor(Math.random() * data.length)]
+                    let locationadjective = locadj.word
+      
+                    this.setState({
+                        locationadjective
+                    });
+                }) 
+    
+                    fetch('http://localhost:8000/words?type=loc-adj&genre=western', options)
+                .then(response => response.json())
+                .then(data => {
+                    let locadj = data[Math.floor(Math.random() * data.length)]
+                    let locationadjective = locadj.word
+      
+                    this.setState({
+                        locationadjective
+                    });
+                }) 
+             
+                fetch('http://localhost:8000/words?type=location&genre=western', options)
+                .then(response => response.json())
+                .then(data => {
+                    let loc = data[Math.floor(Math.random() * data.length)]
+                    let location = loc.word
+      
+                    this.setState({
+                        location
+                    });
+                })              
+            }
+
+            if (this.state.genre == 'genlit') {
+
+                fetch('http://localhost:8000/words?type=sub-adj&genre=lit', options)
+                .then(response => response.json())
+                .then(data => {
+                    let subadj = data[Math.floor(Math.random() * data.length)]
+                    let subjectadjective = subadj.word
+      
+                    this.setState({
+                        subjectadjective
+                    });
+                }) 
+    
+                fetch('http://localhost:8000/words?type=subject&genre=lit', options)
+                .then(response => response.json())
+                .then(data => {
+                    let sub = data[Math.floor(Math.random() * data.length)]
+                    let subject = sub.word
+      
+                    this.setState({
+                        subject
+                    });
+                })  
+    
+                fetch('http://localhost:8000/words?type=loc-adj&genre=lit', options)
+                .then(response => response.json())
+                .then(data => {
+                    let locadj = data[Math.floor(Math.random() * data.length)]
+                    let locationadjective = locadj.word
+      
+                    this.setState({
+                        locationadjective
+                    });
+                }) 
+    
+                    fetch('http://localhost:8000/words?type=loc-adj&genre=lit', options)
+                .then(response => response.json())
+                .then(data => {
+                    let locadj = data[Math.floor(Math.random() * data.length)]
+                    let locationadjective = locadj.word
+      
+                    this.setState({
+                        locationadjective
+                    });
+                }) 
+             
+                fetch('http://localhost:8000/words?type=location&genre=lit', options)
+                .then(response => response.json())
+                .then(data => {
+                    let loc = data[Math.floor(Math.random() * data.length)]
+                    let location = loc.word
+      
+                    this.setState({
+                        location
+                    });
+                })              
+            }
+
+            if (this.state.genre == 'scifi') {
+
+                fetch('http://localhost:8000/words?type=sub-adj&genre=scifi', options)
+                .then(response => response.json())
+                .then(data => {
+                    let subadj = data[Math.floor(Math.random() * data.length)]
+                    let subjectadjective = subadj.word
+      
+                    this.setState({
+                        subjectadjective
+                    });
+                }) 
+    
+                fetch('http://localhost:8000/words?type=subject&genre=scifi', options)
+                .then(response => response.json())
+                .then(data => {
+                    let sub = data[Math.floor(Math.random() * data.length)]
+                    let subject = sub.word
+      
+                    this.setState({
+                        subject
+                    });
+                })  
+    
+                fetch('http://localhost:8000/words?type=loc-adj&genre=scifi', options)
+                .then(response => response.json())
+                .then(data => {
+                    let locadj = data[Math.floor(Math.random() * data.length)]
+                    let locationadjective = locadj.word
+      
+                    this.setState({
+                        locationadjective
+                    });
+                }) 
+    
+                    fetch('http://localhost:8000/words?type=loc-adj&genre=scifi', options)
+                .then(response => response.json())
+                .then(data => {
+                    let locadj = data[Math.floor(Math.random() * data.length)]
+                    let locationadjective = locadj.word
+      
+                    this.setState({
+                        locationadjective
+                    });
+                }) 
+             
+                fetch('http://localhost:8000/words?type=location&genre=scifi', options)
+                .then(response => response.json())
+                .then(data => {
+                    let loc = data[Math.floor(Math.random() * data.length)]
+                    let location = loc.word
+      
+                    this.setState({
+                        location
+                    });
+                })              
+            }
+            
     }
 
     render() {
@@ -129,13 +438,51 @@ class SentenceGeneratorPage extends Component {
         <div className='SentenceGenerator'>
             <header className='appHeader'>
                 <h3>
-                    Suffering from writer's block? 
-                    Write Now is here to help! 
-                    Click the button to get a randomly 
-                    generated sentence you can use as a writing prompt.
+                    Select a genre then click the "write now" button to get a writing prompt in that genre. 
+                    Select the "wild card" to get a sentence that combines elements from all the genres.
                 </h3>
             </header>
             <main> 
+                <div className ='radio'>
+                    <form>
+                        <label>
+                            <input type="radio" value="scifi" name="scifi" 
+                                checked={this.state.genre === 'scifi'}
+                                onChange={this.handleChange}/> 
+                            Sci-Fi 
+                        </label>
+                        <label>
+                            <input type="radio" value="fantasy" name="fantasy" 
+                                checked={this.state.genre === 'fantasy'}
+                                onChange={this.handleChange}/> 
+                            Fantasy
+                        </label>
+                        <label>
+                            <input type="radio" value="horror" name="horror" 
+                                checked={this.state.genre === 'horror'}
+                                onChange={this.handleChange}/> 
+                            Horror
+                        </label>
+                        <label>
+                            <input type="radio" value="western" name="western" 
+                                checked={this.state.genre === 'western'}
+                                onChange={this.handleChange}/> 
+                            Western
+                        </label>
+                        <label>
+                            <input type="radio" value="genlit" name="genlit" 
+                            checked={this.state.genre === 'genlit'}
+                            onChange={this.handleChange}/> 
+                            General Lit
+                        </label>
+                        <label>
+                            <input type="radio" value="wildcard" name="wildcard" 
+                            checked={this.state.genre === 'wildcard'}
+                            onChange={this.handleChange}/> 
+                            Wild Card!
+                        </label>
+                    </form>
+                </div>
                 <SentenceGeneratorForm onSubmit={this.handleFormSubmit}>
                     <button type='submit'>
                         Write now!
@@ -146,7 +493,8 @@ class SentenceGeneratorPage extends Component {
                             Home
                         </button>  
                     </Link> 
-                <h3>{this.state.firstArticle} {this.state.subjectadjective} {this.state.subject} {this.state.verb} {this.state.preposition} {this.state.article} {this.state.locationadjective} {this.state.location}{this.state.punctation} </h3>  
+                    {this.state.loading ? <h1>Loading...</h1>: null}
+                    <h3>{this.state.firstArticle} {this.state.subjectadjective} {this.state.subject} {this.state.verb} {this.state.preposition} {this.state.article} {this.state.locationadjective} {this.state.location}{this.state.punctation} </h3> 
             </main>
        </div>
       );
